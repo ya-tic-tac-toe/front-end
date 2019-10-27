98e29674f656f5421c7b09bcdcf2ff343d746c07
Kyle Peeler <kylepeeler@gmail.com>
2019-10-27 15:40:24 -0400

    add starter screen, imported styled components (#4)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,49 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle, 
+  IonToolbar, 
+} from '@ionic/react';
+import styled from 'styled-components';
 import React from 'react';
+
+const HomeImage = styled.img`
+  width: 200px;
+  height: 200px;
+  margin-left: auto;
+  margin-right: auto;
+  display: block;
+`
+
+const HomeTitle = styled.div`
+  font-size: 32px;
+  font-weight: bold;
+  text-align: center;
+  p, em {
+    font-weight: normal;
+    font-size: 16px;
+  }
+`;
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
+          <IonTitle>ya-tic-tac-toe</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        <HomeTitle>
+          Yet Another Tic-Tac-Toe
+          <p>
+            Built by Kyle Peeler & Nick Hubbard
+          </p>
+          <em>Coming soon!</em>
+        </HomeTitle>
+      
+        <HomeImage src="/assets/ya-ttt-org-icon.png" />
       </IonContent>
     </IonPage>
   );
